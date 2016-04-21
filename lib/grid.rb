7f0bd6302be5855e69fa399a0323a669f487da49
eilw <eirikwiig@gmail.com>
2016-04-21 15:00:22 +0100

    Add methods to Grid

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,2 +1,11 @@
 class Grid
+
+  def leave_scent(direction)
+    @scent = direction
+  end
+
+  def has_scent?(direction)
+    @scent == direction
+  end 
+
 end
---
+++ spec/unit/grid_spec.rb
@@ -0,0 +1,17 @@
+require 'grid'
+
+describe Grid do
+  subject(:grid){described_class.new()}
+
+  describe '#has_scent?' do
+
+    it 'does not have a scent initially' do
+      expect(grid.has_scent?('N')).to be false
+    end
+
+    it 'matches the scent with the direction given' do
+      grid.leave_scent('N')
+      expect(grid.has_scent?('N')).to be true
+    end
+  end
+end
